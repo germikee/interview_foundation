004c2c064d93a4b4fd760716f31270826eb7b373
Germaine Mechili Abellanosa <abellanosa.germaine@gmail.com>
2021-05-25 23:46:54 +0800

    margin on spinner

--- resources/js/components/AddGithubToken.vue
+++ resources/js/components/AddGithubToken.vue
@@ -14,7 +14,7 @@
           <input v-model="token" type="text" name="email" id="email" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full h-10 p-2 mt-3 sm:text-sm border-gray-300 rounded-md" placeholder="you@example.com" />
         </div>
         <button @click="addGithubToken" type="submit" class="mt-3 w-full inline-flex items-center justify-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
-          Save <b-spinner small v-if='loading' label="Loading..." ></b-spinner>
+          Save <b-spinner small v-if='loading' class="mx-2" label="Loading..." ></b-spinner>
         </button>
       </form>
     </div>
--- resources/js/components/StarredRepos.vue
+++ resources/js/components/StarredRepos.vue
@@ -21,7 +21,7 @@
     </div>
     <div v-else>
         <button @click="getStarredRepos" type="button" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-            Retrieve Starred Repositories <b-spinner small v-if='loading' label="Loading..." ></b-spinner>
+            Retrieve Starred Repositories <b-spinner small v-if='loading' class="mx-2" label="Loading..." ></b-spinner>
         </button>
     </div>
 </template>
