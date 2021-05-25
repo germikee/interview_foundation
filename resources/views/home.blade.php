58f170fd4265daa32d8170842eb737425657ec63
Germaine Mechili Abellanosa <abellanosa.germaine@gmail.com>
2021-05-25 23:43:18 +0800

    add spinner

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,22 +4,11 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                </div>
-
-                @if ($token)
-                    <starred-repos token="{{ $token }}" :repositories="{{ json_encode($repositories ?? '') }}"></starred-repos>
-                @else
-                    <add-github-token></add-github-token>
-                @endif
-
-            </div>
+            @if ($token)
+                <starred-repos token="{{ $token }}" :repositories="{{ json_encode($repositories ?? '') }}"></starred-repos>
+            @else
+                <add-github-token></add-github-token>
+            @endif
         </div>
     </div>
 </div>
